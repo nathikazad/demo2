39eec29ed8a993fc2f4c9d04cda894a8bb8673d1
merberich <merberich@gmail.com>
2018-04-22 23:43:56 -0700

    Implement closest-edge finding algorithm

--- demo2.go
+++ demo2.go
@@ -7,12 +7,20 @@ import (
 
 func main() {
 
+/*
 	world := simulator.GetWorldFromFile("4by4.map")
 	path, distance := world.ShortestPath(18, 26)
 	fmt.Println(path, " ", distance)
 	world.SetFrameRate(10)
 	world.AddCars(5)
 	go world.Loop()
+//*/
+
+// TODO: remove this test code in favor of world_test.go
+//*
+  world := simulator.GetWorldFromFile("edgeFindTest.map")
+  testcoords := simulator.Coordinates{X:120, Y:50}
+  intersect, edgeID := world.ClosestEdgeAndCoord(testcoords)
+  fmt.Println("Closest intersect point: ", intersect, "edgeID: ", edgeID)
+//*/
 }
-
-
--- simulator/world.go
+++ simulator/world.go
@@ -4,6 +4,8 @@ import (
 	"demo2/dijkstra"
 	"time"
 	"math/rand"
+  "math"
+  //"fmt"
 )
 
 type Edge struct {
@@ -49,10 +51,109 @@ func (w *World) ShortestPath(n1_id uint, n2_id uint) ([]int, uint){
 	return best.Path, uint(best.Distance)
 }
 
+// TODO: remove commented-out test prints
+// TODO: proper helper function breakdown of ClosestEdgeAndCoord
+// TODO: proper test file world_test.go
+
 // ClosestEdgeAndCoord For coords within world space, find  closest coords on an edge on world graph
+// Return coordinates of closest point on world graph, and corresponding edge ID in world struct
 func (w *World) ClosestEdgeAndCoord(queryPoint Coordinates) (intersect Coordinates, edgeID uint) {
-	// TODO: implement me
-	return
+  // TODO: input sanitation/validation; error handling?
+
+  shortestDistance := math.Inf(1)
+  intersect = Coordinates{0, 0}
+  edgeID = 0
+
+  // TODO: remove randomness caused by traversing equivalent closest edges with 'range' on map here
+  for edgeIdx, edge := range w.edges {
+    coord, dist := edge.checkIntersect(queryPoint)
+    //fmt.Print("[", edgeIdx, "] <ClosestEdgeAndCoord>")
+    //fmt.Print(" query: ", queryPoint, ", shortest: ", shortestDistance, ", new: ", coord, ", dist: ", dist)
+    if dist < shortestDistance {
+      shortestDistance = dist
+      intersect = coord
+      edgeID = edgeIdx
+      //fmt.Print(" (new shortest: ", dist , " @", coord, ")")
+    }
+    //fmt.Println()
+  }
+  return
+}
+
+// checkIntersect find the point on this edge closest to the query coordinates and report distance
+// Return coordinates of closest point and distance of closest point to query
+func (e *Edge) checkIntersect(query Coordinates) (intersect Coordinates, distance float64) {
+  // Via https://stackoverflow.com/a/5205747
+
+  // TODO: cleaner logic in implementation, needs to be several helper functions for testing
+
+  // Query coords as floats for internal math
+  xQuery := float64(query.X)
+  yQuery := float64(query.Y)
+
+  // Edge coordinate references
+  x1 := float64(e.startingNode.coordinates.X)
+  y1 := float64(e.startingNode.coordinates.Y)
+  x2 := float64(e.endingNode.coordinates.X)
+  y2 := float64(e.endingNode.coordinates.Y)
+
+  // Slope of edge and perpendecular section
+  mEdge := float64(y2 - y1) / float64(x2 - x1)
+  mPerp := float64(-1) / mEdge
+
+  //fmt.Print("<checkIntersect>")
+  //fmt.Print(" query: {", xQuery, ",", yQuery, "}")
+  //fmt.Print(", Edge{", x1, ",", y1, "}->{", x2, ",", y2, "}")
+  //fmt.Print(", mEdge: ", mEdge, ", mPerp: ", mPerp)
+
+  inPerp := false  // By default, not in perpendicular region
+  isVert := math.IsInf(mPerp, 0)  // Is the perpendicular section vertical?
+
+  // Determine if the query point lies in region perpendicular to the edge segment
+  if (isVert) {  // Regions determined by x values
+    if ((x1 < x2 && x1 < xQuery && xQuery < x2) || (x2 < x1 && xQuery < x1 && x2 < xQuery)) {
+      inPerp = true
+    }
+  } else {  // Regions determined by y values
+    // Relative y-coordinates of perpendicular lines at bounds of edge segment at x-value of query
+    y1Rel := mPerp * (xQuery - x1) + y1
+    y2Rel := mPerp * (xQuery - x2) + y2
+    if ((y1Rel < y2Rel && y1Rel < yQuery && yQuery < y2Rel) || (y2Rel < y1Rel && yQuery < y1Rel && y2Rel < yQuery)) {
+      inPerp = true
+    }
+  }
+
+  //fmt.Print(", inPerp: ", inPerp, ", isVert: ", isVert)
+
+  if (!inPerp) {  // Not in perpendicular segment or vertical; identify closest
+    distance1 := query.Distance(e.startingNode.coordinates)
+    distance2 := query.Distance(e.endingNode.coordinates)
+    //fmt.Println(", dist1: ", distance1, ", dist2: ", distance2)
+    if (distance1 < distance2) {
+      intersect = e.startingNode.coordinates
+      distance = distance1
+    } else {
+      intersect = e.endingNode.coordinates
+      distance = distance2
+    }
+  } else {  // In perpendicular region; find intersection on Edge
+    // Check for straight lines to ease calculations
+    if (x1 == x2) {
+      intersect.X = uint(x1)
+      intersect.Y = uint(yQuery)
+
+    } else if (y1 == y2) {
+      intersect.X = uint(xQuery)
+      intersect.Y = uint(y1)
+    } else {
+      intersect.X = uint((mEdge * x1 - mPerp * xQuery + yQuery - y1) / (mEdge - mPerp))
+      intersect.Y = uint(mPerp * (float64(intersect.X) - xQuery) + yQuery)
+    }
+    distance = intersect.Distance(query)
+  }
+  //fmt.Print(", intersect: ", intersect, ", distance: ", distance)
+  //fmt.Println()
+  return
 }
 
 func (w *World) SetFrameRate(frameRate uint) {
